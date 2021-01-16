477600e65cba0ff3f847bc4248fd16a63da2010b
Ihor Khvalbota Software Engineer <ihorkhvalbota@gmail.com>
2021-01-16 14:57:08 +0200

    - cover additional cases

--- src/Logic.ts
+++ src/Logic.ts
@@ -1,8 +1,8 @@
 
 
 export  function run() {
-    const res1 = distribute([25, 25, 25, 25], 0, 60) // [85,0,0,15] <br/>
-    const res2 = distribute([25, 25, 25, 25], 2, -20) // [45,25,5,25] <br />
+    const res1 = distribute([25, 25, 25, 25], 0, 60) // [85,0,0,15]
+    const res2 = distribute([25, 25, 25, 25], 2, -20) // [45,25,5,25]
     const res3 = distribute([25, 25, 25, 25], 1, 120) // [0,100,0,0]
 
     console.log(res1)
@@ -17,7 +17,18 @@ function distribute(arr: number[], index: number, val: number) {
     if(val + arr[index] >= maxValue || val + arr[index] < 0){
         return arr.map((_: number, i: number) => i === index ? 100 : 0)
     }
-    return  arr
 
+    arr[index] += val
+    for(let i = 0; i < arr.length; i++){
+        if(i===index){continue}
+        if(arr[i] > val){
+            arr[i] -= val
+            break
+        }else {
+            val -= arr[i]
+            arr[i] = 0
+        }
+    }
+    return  arr
 }
 
